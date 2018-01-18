01ed4d0c25b047c4f0559e97d96734044f296d7a
FilipMiljakovic <filipmiljakovic1994@gmail.com>
2018-01-19 00:39:48 +0100

    akcije za tabelu photo_comments

---
+++ server/photo_comments_pdo.php
@@ -0,0 +1,101 @@
+<?php
+
+include 'connection.php';
+
+// dohvatanje svih komentara jedne slike po photoId
+function getPhotoComments($db, $photoId){
+
+    $query="select *
+            from centralcemeteries.photo_comments 
+            where photoId=:photoId
+            order by time desc";
+
+    $stmt=$db->prepare($query);
+
+    $stmt->bindParam(":photoId", $photoId, PDO::PARAM_INT);
+
+    if($stmt->execute()){
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+    else{
+        return null;
+    }
+}
+
+// unosenje novog komentara
+function insertPhotoComment($db, $photoId, $username, $email, $text){
+
+    $db->beginTransaction();
+
+    $query = "insert into centralcemeteries.photo_comments (photoId, username, email, text)
+              values(:photoId, :username, :email, :text)";
+
+    $stmt = $db->prepare($query);
+
+    $stmt->bindParam(":photoId", $photoId, PDO::PARAM_INT);
+    $stmt->bindParam(":username", $username, PDO::PARAM_STR);
+    $stmt->bindParam(":email", $email, PDO::PARAM_STR);
+    $stmt->bindParam(":text", $text, PDO::PARAM_STR);
+
+    if($stmt->execute()){
+        $db->commit();
+        return true;
+    }
+    else{
+        $db->rollback();
+       return false;
+    }
+}
+
+// Brisanje komentara
+function deletePhotoComment($db, $id){
+
+    $db->beginTransaction();
+
+    $query="delete from centralcemeteries.photo_comments
+            where id=:id";
+
+    $stmt=$db->prepare($query);
+
+    $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+
+    if($stmt->execute()){
+        $db->commit();
+        return true;
+    }
+    else{
+        $db->rollback();
+        return false;
+    }
+}
+
+
+try{
+$pdo=Connection::getConnectionInstance();
+
+    //getAllPhotoComments test
+ //   $photoId=1;
+ //   $photoComments=getPhotoComments($pdo,$photoId);
+ //   var_dump($photoComments);
+
+    // insertPhotoComment test
+  //  $photoId = 1;
+  //  $username = "test_korisnik";
+  //  $email = "test@test.com";
+  //  $text = "Mnogo lepa slika";
+  //  $inserted = insertPhotoComment($pdo, $photoId, $username, $email, $text);
+  //  echo $inserted;
+
+    // deletePhotoComment test
+    $id = 1;
+    $deleted = deletePhotoComment($pdo, $id);
+    echo $deleted;
+
+
+}catch(PDOException $e){
+echo $e->getMessage();
+unset($pdo);
+}
+
+
+?>
