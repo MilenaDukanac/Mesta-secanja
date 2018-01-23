6d40b4f9d786facc1d6ab6e927effae37852dac2
NatalijaKom <natali943105@gmail.com>
2018-01-23 17:29:20 +0100

    ubaceno i zaglavlje za neregistrovanog korisnika

--- client/about.php
+++ client/about.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
-
-if($_SESSION['type']==="admin")
+if(!isset($_SESSION['type'])) {
+    include 'headerGuest.php';
+}
+else if($_SESSION['type']==="admin")
     include 'headerAdmin.php';
 else if($_SESSION['type']==="other")
     include 'headerOther.php';
