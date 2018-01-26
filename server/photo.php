509f9923ec7d0561a3556323bdf5e4807fb06e9b
NatalijaKom <natali943105@gmail.com>
2018-01-26 23:05:55 +0100

    obradjeni svi zahtevi

--- server/photo.php
+++ server/photo.php
@@ -1,6 +1,5 @@
 <?php
 
-
 include "photo_pdo.php";
 
 $status_messages=array(
@@ -26,65 +25,110 @@ $method=strtoupper($_SERVER['REQUEST_METHOD']);
 if(!in_array($method,$supported_methods)) {
     $response->status=405;
 }
+else {
 
-$pdo = Connection::getConnectionInstance();
+    /* Obrada podrzanih zahteva */
 
-try{
-    switch ($method){
+    $number_of_url_elements = 0;
+    $url_elements = array();
 
-        case "GET":
-
-            if($_GET['type'] == 'cemetery') {
-                $id = $_GET["cemeteryId"];
-
-
-                $data = getAllCemeteryPhotos($pdo, $id);
-                if ($data == null) {
-                    $response->status = 404;
-                    $response->data = null;
-                }
-                else {
-                    $response->status = 200;
-                    $response->data = $data;
-                }
-
-            }
-            else{
-                $response->status = 400;
-                $response->data = null;
-            }
-            break;
-
-        case "POST":
-            //TODO
-            break;
-
-        case "PUT":
-            //TODO
-
-            break;
-
-        case "DELETE":
-            //TODO
-
-            break;
+    if (isset($_SERVER['PATH_INFO'])) {
+        $url_elements = explode("/", $_SERVER['PATH_INFO']);
+        $number_of_url_elements = count($url_elements) - 1;
     }
 
 
+    $pdo = Connection::getConnectionInstance();
+
+    try {
+        switch ($method) {
+
+            case "GET":
+
+                switch ($number_of_url_elements) {
+                    case 2:
+                        // GET/photo/{photoId}
+                        if ($url_elements[1] == "photo") {
+                            $response->data = getPhotoInfo($pdo, intval($url_elements[2]));
+                            $response->status = 200;
+                        } // GET/photos/{cemeteryId}
+                        else if ($url_elements[1] == "photos") {
+                            $response->data = getAllCemeteryPhotos($pdo, intval($url_elements[2]));
+                            $response->status = 200;
+
+                        } else {
+                            $response->status = 400;
+                            $response->data = null;
+                        }
+                        break;
+
+                }
+                break;
+            case "POST":
+                $newPhoto = json_decode(file_get_contents("php://input"));
+
+                var_dump($newPhoto);
+                echo "post";
+
+                $tmpId = insertPhoto($pdo, $newPhoto->name, $newPhoto->cemeteryId, $newPhoto->author, $newPhoto->year, $newPhoto->note, $newPhoto->longitude, $newPhoto->latitude);
+                if ($tmpId != null) {
+                    $response->data = $tmpId;
+                    $response->status = 201;
+                    echo "ubacio";
+                } else {
+                    echo "greska";
+                    $response->status = 400;
+                    $response->data = null;
+                }
+                break;
 
 
-}catch(Exception $e){
-    $response->status=500;
-    $response->error=true;
-    $response->data=null;
+            case "DELETE":
+
+                // DELETE/photo/{photoId}
+
+                if($number_of_url_elements == 2 and $url_elements[1] == "photo") {
+
+                    if (deletePhoto($pdo, intval($url_elements[2]))) {
+                        $response->data = "true";
+                        $response->status = 200;
+                    } else {
+                        $response->data = null;
+                        $response->status = 400;
+                    }
+                }
+                else{
+                    $response->status = 400;
+                    $response->data = null;
+                }
+
+                break;
+        }
+
+    } catch (Exception $e) {
+        $response->status = 500;
+        $response->error = true;
+        $response->data = null;
+    }
 }
+// zaglavlje
 
+/*   if(headers_sent()){
+    echo "Ne moze!";
+}*/
+//   else {
 
+// Menjamo promenljivama
 header("HTTP/1.1 " . $response->status . " " . $status_messages[$response->status]);
+// Dodajemo da je u json formatu
 header("Content-Type:application/json");
+//}
 
+
+// telo
 if($response->data != null){
     echo json_encode($response->data);
+
 }
 
 
