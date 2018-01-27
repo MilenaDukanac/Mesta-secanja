204535446511341999089c5f9075bdcf1fe1663b
MilenaDukanac <sekapersa8@gmail.com>
2018-01-27 20:53:18 +0100

    izmene vezane za dodavanje vise od jedne drzave, clearMsg

--- client/newCountry.js
+++ client/newCountry.js
@@ -4,6 +4,9 @@ newCountry.controller('newCountryController', ['$scope', '$http', '$window', fun
 		$scope.countryName = "";
 		$scope.newCountryMessage = "";
 		$scope.showNewCountryMessage = false;
+		$scope.clearMsg = function() {
+			$scope.newCountryMessage = "";
+		}
 
     $scope.insertCountry = function () {
 				var country = angular.toJson($scope.countryName);
@@ -15,10 +18,12 @@ newCountry.controller('newCountryController', ['$scope', '$http', '$window', fun
         }).then(function successHandler(result) {
 						$scope.newCountryMessage = "New country is successfully added.";
 						$scope.showNewCountryMessage = true;
+						document.formCountry.reset();
             console.log(result);
         }, function errorHandler(result) {
 						$scope.newCountryMessage = "Please try again.";
 						$scope.showNewCountryMessage = true;
+						document.formCountry.reset();
             console.log(result);
         });
     };
