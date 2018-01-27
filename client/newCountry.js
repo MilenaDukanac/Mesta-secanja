257c50d7695d9f32fa4c972fbd1886c7cab612ac
NatalijaKom <natali943105@gmail.com>
2018-01-27 23:04:35 +0100

    Poruka za neuspesno unosenje

--- client/newCountry.js
+++ client/newCountry.js
@@ -1,29 +1,29 @@
 var newCountry = angular.module('newCountry', []);
 
 newCountry.controller('newCountryController', ['$scope', '$http', '$window', function ($scope, $http, $window) {
-		$scope.countryName = "";
-		$scope.newCountryMessage = "";
-		$scope.showNewCountryMessage = false;
-		$scope.clearMsg = function() {
-			$scope.newCountryMessage = "";
-		}
+    $scope.countryName = "";
+    $scope.newCountryMessage = "";
+    $scope.showNewCountryMessage = false;
+    $scope.clearMsg = function() {
+        $scope.newCountryMessage = "";
+    }
 
     $scope.insertCountry = function () {
-				var country = angular.toJson($scope.countryName);
+        var country = angular.toJson($scope.countryName);
 
         $http({
             method: "POST",
             url: '../server/country.php',
-						data: country
+            data: country
         }).then(function successHandler(result) {
-						$scope.newCountryMessage = "New country is successfully added.";
-						$scope.showNewCountryMessage = true;
-						document.formCountry.reset();
+            $scope.newCountryMessage = "New country is successfully added.";
+            $scope.showNewCountryMessage = true;
+            document.formCountry.reset();
             console.log(result);
         }, function errorHandler(result) {
-						$scope.newCountryMessage = "Please try again.";
-						$scope.showNewCountryMessage = true;
-						document.formCountry.reset();
+            $scope.newCountryMessage = "Country already exists! ";
+            $scope.showNewCountryMessage = true;
+            document.formCountry.reset();
             console.log(result);
         });
     };
