06a05b88d1001268458958481de6aa4d025f4253
MilenaDukanac <sekapersa8@gmail.com>
2018-01-27 21:16:18 +0100

    Odgovarajuca funkcionalnost za neispravni unos username/pass

--- client/login.js
+++ client/login.js
@@ -4,10 +4,14 @@ app.controller('loginControler', ['$scope', '$http', '$window', function ($scope
 
     $scope.username = "";
     $scope.password = "";
-
+	$scope.loginMessage="";
+	$scope.showLoginMessage=false;
+	$scope.showLoginIndicator=true;
+	
     $scope.login = function () {
         var user = $scope.username;
         var pass = $scope.password;
+	
 
         // $http.get('../server/user1.php?type=login&username=' + user + '&password=' + pass, {responsetype: JSON}).
         // success(function(data, status, headers, config){
@@ -26,11 +30,15 @@ app.controller('loginControler', ['$scope', '$http', '$window', function ($scope
             method: "GET",
             url: '../server/user.php?type=login&username=' + user + '&password=' + pass
         }).then(function successHandler(result) {
+			$scope.showLoginIndicator=false;
             $window.location.href = "home.php";
             console.log(result);
 
         }, function errorHandler(result) {
-
+			//$scope.loginMessage="Incorrect username or password!";
+			
+			$scope.showLoginMessage=true;
+			$scope.showLoginIndicator=true;
             console.log(result);
         });
     };
