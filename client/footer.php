e10fd7d83a7b40d1d026eb71b4c0064af748c079
FilipMiljakovic <filipmiljakovic1994@gmail.com>
2018-01-27 20:44:47 +0100

    Dodato slanje mail-a u footer-u
    Mail server mora da se podesi na svakom kompu posebno, tako da samo ovako nece da radi. Podesavanja imate u onom mailu  koji je Andjelka poslala, ali gmail ima problem sa time da ne dozvoljava da joj aplikacije tek tako pristupaju. Poslali smo Andjelki mail, da vidimo kako i da li to moze da se prevazidje.

--- client/footer.php
+++ client/footer.php
@@ -27,6 +27,48 @@
 					</div>
 
 					<div class="col-sm-6">
+						<?php
+						use PHPMailer\PHPMailer\PHPMailer;
+						use PHPMailer\PHPMailer\Exception;
+
+						if(isset($_POST['submit'])){
+							$email = $_POST['email'];
+							$message = $_POST['message'];
+
+							//Load composer's autoloader
+							require 'vendor/autoload.php';
+
+							$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+							try {
+									//Server settings
+									//$mail->SMTPDebug = 2;                                 // Enable verbose debug output
+
+									//Ovde treba da se podesi mail protokol i server, za svaki racunar mora posebno
+									$mail->isSMTP();                                      // Set mailer to use SMTP
+									$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+									$mail->SMTPAuth = true;                               // Enable SMTP authentication
+									$mail->Username = $email;                 // SMTP username
+									$mail->Password = '';                           // SMTP password
+									$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+									$mail->Port = 587;                                    // TCP port to connect to
+
+									//Recipients
+									$mail->setFrom($email);  // treablo bi onaj koji salje mail
+									$mail->addAddress('');  //Onaj koji prima mail
+
+									//Content
+									$mail->isHTML(true);                                  // Set email format to HTML
+									$mail->Subject = 'Here is the subject';
+									$mail->Body    = $message;
+									$mail->AltBody = '';
+
+									$mail->send();
+									echo 'Message has been sent';
+							} catch (Exception $e) {
+									echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+							}
+						}
+								?>
 						<form action="#" method="post">
 							<div class="form-group">
 								<label class="form-control-label" for="contacts4-3-email">Email<span class="form-asterisk">*</span></label>
@@ -39,7 +81,7 @@
 							</div>
 
 							<div class="mbr-buttons mbr-buttons--right btn-inverse">
-								<button type="submit" class="btn btn-sm btn-black">
+								<button type="submit" name="submit" class="btn btn-sm btn-black">
 									Contact us
 								</button>
 							</div>
