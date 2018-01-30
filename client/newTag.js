a52b7f0c5a50028fc70202bbc25587bbcadd042e
milos494 <milosloncarevic494@gmail.com>
2018-01-30 01:20:22 +0100

    dodavanje taga sa mogucim vrednstima

--- client/newTag.js
+++ client/newTag.js
@@ -4,28 +4,41 @@ newTag.controller('newTagController', ['$scope', '$http', '$window', function ($
     $scope.newTag = {};
     $scope.newTagMessage = "";
     $scope.showNewTagMessage = false;
-	$scope.clearMsg = function() {
-		$scope.newTagMessage = "";
-	}
-	
+    $scope.clearMsg = function() {
+        $scope.newTagMessage = "";
+    }
+
+
+
     $scope.insertTag = function () {
+
+        var possibleValues = [];
+        var pvArray = $scope.newTag.possibleValues.split(';');
+
+
+
+        $scope.newTag.possibleValuesArray = pvArray;
+
+
         var tag = angular.toJson($scope.newTag);
 
         $http({
-          method: "POST",
-          url: '../server/tags.php',
-					data: tag
+            method: "POST",
+            url: '../server/tags.php',
+            data: tag
         }).then(function successHandler(result) {
-		    	$scope.newTagMessage = "New tag is successfully added.";
-		    	$scope.showNewTagMessage = true;
-				document.formTag.reset();
-          console.log(result);
+            $scope.newTagMessage = "New tag is successfully added.";
+            $scope.showNewTagMessage = true;
+            document.formTag.reset();
+            console.log(result);
         }, function errorHandler(result) {
-		    	$scope.newTagMessage = "The category doesn't exist or the tag is already added!";
-		    	$scope.showNewTagMessage = true;
-				document.formTag.reset();
-          console.log(result);
+            $scope.newTagMessage = "The category doesn't exist or the tag is already added!";
+            $scope.showNewTagMessage = true;
+            document.formTag.reset();
+            console.log(result);
         });
     };
 
 }]);
+
+
